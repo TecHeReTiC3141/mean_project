06832a07e4cbb2607617426407a9955498c8459e
TecHeReTiC <talex73@mail.ru>
2023-04-29 22:20:06 +0500

    Mongoose has been installed + layout has been created

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + 'views');
-app.set('layout', 'layouts/layout');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout'); // for header and footer templates
 app.use(expressLayouts);
+
 app.use(express.static('public'));
 
-const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
 
 app.use('/', indexRouter);
 
